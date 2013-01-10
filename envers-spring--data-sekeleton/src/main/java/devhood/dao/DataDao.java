8193bf3b88c62ff0820aab2319b8d80d4ae435e0
flo <fuchsfl@googlemail.com>
2013-01-10 21:44:44 +0100

    Skeleton JPA Spring Data, Hibernate 4 with Envers

--- envers-spring--data-sekeleton/src/main/java/devhood/dao/DataDao.java
+++
@@ -1,9 +0,0 @@
-package devhood.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import devhood.model.Data;
-
-public interface DataDao extends JpaRepository<Data, Long> {
-
-}
