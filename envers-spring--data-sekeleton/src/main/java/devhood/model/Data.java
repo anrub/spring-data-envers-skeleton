283ba860ac499a32326c38a966953ad14f74b450
flo <fuchsfl@googlemail.com>
2013-01-10 21:30:31 +0100

    Skeleton JPA Spring Data, Hibernate 4 with Envers

---
+++ envers-spring--data-sekeleton/src/main/java/devhood/model/Data.java
@@ -0,0 +1,46 @@
+package devhood.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.envers.Audited;
+
+@Audited
+@Entity
+public class Data {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String name;
+
+	private String givenname;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGivenname() {
+		return givenname;
+	}
+
+	public void setGivenname(String givenname) {
+		this.givenname = givenname;
+	}
+
+}
